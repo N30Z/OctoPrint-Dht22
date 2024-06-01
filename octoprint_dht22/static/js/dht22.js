50a4189ba0a28564177ffc3c3c51a543948ce1b4
buttl <jan@feddern.test>
2024-06-01 17:25:39 +0200

    function()addLogMessage
    TODO:
    Settings updaten
    Icons with Font?

--- octoprint_dht22/static/js/dht22.js
+++ octoprint_dht22/static/js/dht22.js
@@ -13,6 +13,7 @@ $(function() {
                 $("#dht22_log").html(response);
             },
             error: function(xhr, status, error) {
+                addLogMessage("Failed to fetch data from Arduino.");
                 console.error("Failed to fetch data from Arduino:", error);
             }
         });
@@ -28,11 +29,29 @@ $(function() {
 
             $("#navbar_temperature").text(temperature.toFixed(2));
             $("#navbar_humidity").text(humidity.toFixed(2));
+            addLogMessage("Data fetched successfully from: " + apiUrl);
+            addLogMessage("Temperature: " + temperature + "°C, Humidity: " + humidity + "%");
         } else {
+                $("#navbar_temperature").text("--");
+                $("#navbar_humidity").text("--");
+                addLogMessage("Failed to parse data from: " + apiUrl);
+        }
+        }).fail(function() {
             $("#navbar_temperature").text("--");
             $("#navbar_humidity").text("--");
-        }
+            addLogMessage("Failed to fetch data from: " + apiUrl);
+        });
     }
+
+    function addLogMessage(message) {
+        var iframe = document.getElementById("dht22_tab").querySelector("iframe");
+        var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+        var logElement = iframeDoc.getElementById("dht22_log");
+        var currentTime = new Date().toLocaleTimeString();
+        logElement.innerHTML += "<div>[" + currentTime + "] " + message + "</div>";
+        logElement.scrollTop = logElement.scrollHeight;
+        }
+
 
     fetchArduinoData();
     setInterval(fetchArduinoData, 10000); // Default refresh rate of 10 seconds
